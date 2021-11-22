a8672f509545558e5dc71da383ba516788887f8a
Manreet Grewal <mank108.g@gmail.com>
2021-11-22 12:39:19 +0530

    Update census_app.py

--- census_app.py
+++ census_app.py
@@ -50,13 +50,14 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Add title on the main page and in the sidebar.
 st.title("ADULT CENSUS DATA")
-st.sidebar.title("Visulisation")
+st.sidebar.title("Menu")
 
 # Using the 'if' statement, display raw data on the click of the checkbox.
 if st.sidebar.checkbox("Show raw data"):
   st.subheader("Census Data Set")
   st.dataframe(census_df)
-  st.write(f"The shape of the dataframe is {census_df.shape}")
+  st.write(f"The number of rows in the dataframe are {census_df.shape[0]}")
+  st.write(f"The number of columns in the dataframe are {census_df.shape[1]}")
 
 # Add a multiselect widget to allow the user to select multiple visualisations.
 # Add a subheader in the sidebar with the label "Visualisation Selector"
@@ -67,16 +68,32 @@ st.sidebar.subheader("Visualisation Selector")
 plot_list = st.sidebar.multiselect("Select plot type",('Histogram', 'Box Plot', 'Count Plot', 'Pie Chart', 'Correlation Heatmap', 'Pair Plot'))
 
 # Display pie plot using matplotlib module and 'st.pyplot()'
-if 'Pie Chart' in plot_types:
+if 'Pie Chart' in plot_list:
   st.subheader("Pie Chart")
-  pie_data = census_df['income-group'].values_counts()
-  plt.figure(figsize = (12,6))
-  plt.title(f"Pie Chart for Income Group")
-  plt.pie(pie_data,labels = pie_data.index,startangle = 30,explode = np.linspace(0.06,0.16,6))
-  st.pyplot()
+  column = st.sidebar.multiselect("Select the column for pie chart", ('income','gender'))
+  for i in column:
+    pie_data = census_df[i].values_counts()
+    plt.figure(figsize = (5,5))
+    plt.title(f"Distribution of records for different {i} groups")
+    plt.pie(pie_data,labels = pie_data.index, autopct = '%1.2f%%',startangle = 30,explode = np.linspace(.01,.05,len(pie_data)))
+    st.pyplot()
   
-  plt.figure(figsize = (12,6))
-  plt.title(f"Pie Chart for Gender")
-  pie_data1 = census_df['gender'].values_counts()
-  plt.pie(pie_data1,labels = pie_data1.index,startangle = 30,explode = np.linspace(0.06,0.16,6))
-  st.pyplot()
+
+
+# Display box plot using matplotlib module and 'st.pyplot()'
+if 'Box Plot' in plot_list:
+  st.subheader("Box Plot")
+  hue_columns = st.sidebar.multiselect("Select column for grouping the distribution of records",('income','gender'))
+  for i in hue_columns:
+
+    plt.figure(figsize = (12,6))
+    plt.title(f"distributions of hour worked per week for different {i} groups")
+    sns.boxplot(census_df['hours-per-week'], y = census_df[i])
+    st.pyplot()
+
+# Display count plot using seaborn module and 'st.pyplot()' 
+if 'Count Plot' in plot_list:
+    st.subheader("Count plot for distribution of records for unique workclass groups")
+    plt.figure(figsize = (12,5))
+    sns.countplot(x = 'workclass', hue = 'income', data = census_df)
+    st.pyplot()
